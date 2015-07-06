789a31390072efe605748db923f8db43ef655d88
zack Frazier <zfrazier@gmail.com>
2015-07-06 11:48:42 -0500

    flexbox experiment

--- src/js/hi.js
+++ src/js/hi.js
@@ -1,1 +1,11 @@
 console.log('Oh hey, you’re the best!');
+
+var thestrong = [
+  'Catherine Janemway',
+  'Abby Lee Miller',
+  'Justin Timberlake',
+  'Norman Rockwell',
+  'Phil Collins',
+  'Imperator Furiosa',
+  'Abby Wambach'
+]
